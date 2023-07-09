8052900eec98c0aeb52d69e5d2e06bf96cce512d
jeexare <74197866+jeexare@users.noreply.github.com>
2023-07-09 15:34:40 -0600

    Got the extension to work with chrome.storage

---
+++ snipper/snippert/content.js
@@ -0,0 +1,62 @@
+function handleTextExpansion(event, key) {
+	const input = event.target;
+	const text = input.value;
+  
+	const words = text.split(" ");
+	const cursor = input.selectionStart;
+	let abbreviation = "";
+  
+	for (let i = words.length - 1; i >= 0; i--) {
+	  const word = words[i];
+	  abbreviation = word;
+	  break;
+	}
+  
+	chrome.storage.local.get('expansions', function (data) {
+	  const expansions = data.expansions || {};
+  
+	  if (expansions.hasOwnProperty(abbreviation) && key === "space") {
+		const expansionValue = expansions[abbreviation];
+		let expandedText = "";
+		if (abbreviation.startsWith("=")) {
+			 expandedText = eval(expansionValue);
+		} else {expandedText = expansionValue; }
+
+		const expandedTextLength = expandedText.length;
+  
+		const newText =
+		  text.substring(0, cursor - abbreviation.length) +
+		  expandedText +
+		  text.substring(cursor);
+  
+		input.value = newText;
+	  }
+	});
+  }
+  
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+	if (namespace === 'local' && changes.expansions) {
+	  const newExpansions = changes.expansions.newValue || {};
+	  expansions = newExpansions;
+	}
+  });
+  
+  const inputFields = document.querySelectorAll("input[type='text'], textarea");
+  inputFields.forEach((field) => {
+	field.addEventListener("input", function (event) {
+	  const key = findKey(event);
+	  handleTextExpansion(event, key);
+	});
+  
+	field.addEventListener("keydown", function (event) {
+	  const key = findKey(event);
+	  handleTextExpansion(event, key);
+	});
+  });
+  
+  function findKey(event) {
+	if (event.keyCode == 13 && event.ctrlKey) {
+	  return "space";
+	}
+  }
+  
---
+++ snipper/snippert/snipper.js
@@ -0,0 +1,117 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+/*********************
+       CONSTANTS
+*********************/
+const contentInput = document.getElementById("contentInput");
+const abbInput = document.getElementById("abbInput");
+const addButton = document.getElementById("addButton");
+const snippetsList = document.getElementById("snippetsList");
+
+/********************************
+	          TABS
+*********************************/
+document.addEventListener("click", (e) => {
+	const menuButton = e.target.closest(".tabbutton");
+	if (!menuButton) return; // If it's null/empty return
+
+	const menuName = menuButton.getAttribute("data-tab");
+	if (!menuName) return; // If it's null/empty return
+
+	const menu = document.querySelector(`.tab[data-tab="${menuName}"]`);
+	if (!menu) return; // If it's null/empty return
+
+	const activeButton = document.querySelector(
+		`.tabbutton[data-tab="${menuName}"]`
+	);
+	if (!activeButton) return; // If it's null/empty return
+
+	document
+		.querySelectorAll(".tab")
+		.forEach((menu) => menu.classList.remove("active"));
+
+	menu.classList.add("active");
+
+	document
+		.querySelectorAll(".tabbutton")
+		.forEach((activeButton) => activeButton.classList.remove("active"));
+
+	activeButton.classList.add("active");
+});
+
+  
+/*********************
+     NEW SNIPPET
+*********************/
+function addNewSnippet(newContent, abbreviation) {
+	chrome.storage.local.get('expansions', function (data) {
+	  const expansions = data.expansions || {};
+	  expansions[abbreviation] = newContent;
+	  
+	  chrome.storage.local.set({ expansions: expansions }, function () {
+		displaySnippets();
+	  });
+	});
+  }
+  
+  function deleteSnippet(abbreviation) {
+	chrome.storage.local.get('expansions', function (data) {
+	  const expansions = data.expansions || {};
+	  delete expansions[abbreviation];
+  
+	  chrome.storage.local.set({ expansions: expansions }, function () {
+		displaySnippets();
+	  });
+	});
+  }
+  
+
+  function displaySnippets() {
+	chrome.storage.local.get('expansions', function (data) {
+	  const expansions = data.expansions || {};
+  
+	  snippetsList.innerHTML = "";
+  
+	  for (let abbreviation in expansions) {
+		const expandedText = expansions[abbreviation];
+		const snippetContainer = document.createElement("div");
+  
+		// Create the snippet content element
+		const snippetContent = document.createElement("div");
+		snippetContent.textContent = `${abbreviation} - ${expandedText}`;
+  
+		// Create the delete button element
+		const deleteButton = document.createElement("button");
+		deleteButton.textContent = "x";
+		deleteButton.addEventListener("click", () => {
+		  deleteSnippet(abbreviation);
+		});
+  
+		// Append snippet content and delete button to snippet container
+		snippetContainer.appendChild(snippetContent);
+		snippetContainer.appendChild(deleteButton);
+  
+		snippetsList.appendChild(snippetContainer);
+	  }
+	});
+  }
+  
+
+addButton.addEventListener("click", () => {
+	const newContent = contentInput.value;
+	const newAbb = abbInput.value;
+
+	if (newContent && newAbb) {
+		addNewSnippet(newContent, newAbb);
+		contentInput.value = "";
+		abbInput.value = "";
+	}
+});
+
+
+/********************
+ * DOM
+ */
+
+document.getElementById("onbutton").click();
+displaySnippets();
+});
